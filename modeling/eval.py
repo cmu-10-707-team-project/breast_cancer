c61b4ce999466a1cccbe3112c0b131bb14c43e5c
Allen <allendinghamilton@gmail.com>
2019-04-30 22:46:26 -0400

    evluatation

--- modeling/eval.py
+++ modeling/eval.py
@@ -8,13 +8,12 @@
 #   I M P O R T S   #
 # # # # # # # # # # #
 import argparse
-from datetime import datetime
-import os 
-import sys
-import pandas as pd
+import os
+
+from modeling.model_factory import get_model
+
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-from modeling.data_loader import KerasTestDataGenerator
-from modeling import *
+from modeling.data_loader import KerasDataGenerator
 
 # # # # # # # # # # # # #
 #   F U N C T I O N S   #
@@ -26,6 +25,7 @@ if __name__=="__main__":
 
 	parser.add_argument('--tensorboard-dir', type=str, default='data/log')
 	parser.add_argument('--model', type=str, required=True)
+	parser.add_argument('--saved-model', type=str, required=True)
 	parser.add_argument('--workers', type=int, default=cpu_count())
 
 	parser.add_argument('--test-index-file-path', type=str, required=True)
@@ -34,21 +34,15 @@ if __name__=="__main__":
 	arg = parser.parse_args()
 
 	# use model to predict test data
-	model = unet()
-	timestamp = datetime.now().strftime('%m-%d-%H%M%S')
+	model = get_model(model_name=arg.model_name)
 	model.load_weights(arg.model)
-	tensorboard = TensorBoard(
-		log_dir=arg.tensorboard_dir, write_grads=False, write_images=False)
 
 	print ('...start testing...')
-	test_ds = KerasTestDataGenerator(test_folder = arg.test_folder)
-	df = pd.read_csv(arg.test_index_file_pth)
-	num_test_data = df.shape[0]
-	results = model.predict_generator(test_ds(),steps=num_test_data,verbose=1)
-	print ('...end testing...')
+	test_ds = KerasDataGenerator(
+		index_filepath=arg.test_index_file_path, input_folder=arg.test_folder,
+		labeled=True, mask=arg.model == 'unet')
 
-	# update the index file
-	#num_test_data= results.shape[0]
-	prob = np.average(results.reshape(num_test_data,-1),axis=1)
-	df['tumor_prob'] = pd.Series(prob,index=df.index)
-	df.to_csv(arg.test_index_file_pth, sep=',',index = False)
+	results = model.evaluate_generator(
+		test_ds(), steps=test_ds.steps_per_epoch, verbose=1)
+
+	print(results)
